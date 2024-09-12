e7679eb4b898881d6f73fa82f0e0d7bc1a33dc28
VaideshWaranR <221001169@rajalakshmi.edu.in>
2024-09-12 16:05:43 +0530

    'centered the text'

--- src/Components/Postponed.jsx
+++ src/Components/Postponed.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Postponed = () => {
   return (
-    <div className='text-red-500 text-4xl'>Event Got postponed</div>
+    <div className='text-red-500 text-4xl w-[100%] text-center'>Event Got postponed</div>
   )
 }
 
