6084527ec2ee2f936d494d92c31b019ec5cefa32
VaideshWaranR <221001169@rajalakshmi.edu.in>
2024-11-05 18:46:03 +0530

    rules button

--- src/Components/Cardlayout.jsx
+++ src/Components/Cardlayout.jsx
@@ -1,10 +1,15 @@
-import React, { useCallback, useState } from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
+import React, { useState } from 'react';
 
 const EventCard = React.memo(({ event, index }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  // Toggle popup state for viewing rules
   const togglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
   };
+
   return (
     <div
       key={index}
@@ -32,97 +37,31 @@ const EventCard = React.memo(({ event, index }) => {
         </div>
       )}
 
-      {/* Event Rules */}
-      {event.rules && (
-        <div className="my-4 text-blue-200 text-left flex-grow">
-          <h3 className="font-semibold text-violet-400">Rules:</h3>
-          <ul className="list-disc list-inside">
-            {event.rules.map((rule, ruleIndex) => (
-              <li key={ruleIndex} className="text-white my-1">
-                {rule}
-              </li>
-            ))}
-          </ul>
-        </div>
+      {/* Rules Button */}
+      {event.rulesImage && (
+        <button
+          onClick={togglePopup}
+          className="mt-4 relative px-6 py-2.5 font-medium text-white group"
+        >
+          <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-violet-200 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+          <span className="absolute inset-0 w-full h-full border-2 bg-violet-800 group-hover:bg-violet-600"></span>
+          <span className="relative text-white group-hover:text-white">View Rules</span>
+        </button>
       )}
 
-      {/* Judging Criteria */}
-      {event.judgingCriteria && (
-        <div className="my-4 text-blue-200 text-left flex-grow">
-          <h3 className="font-semibold text-violet-400">Judging Criteria:</h3>
-          <ul className="list-disc list-inside">
-            {event.judgingCriteria.map((criteria, criteriaIndex) => (
-              <li key={criteriaIndex} className="text-white my-1">
-                {criteria}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-{/* {event.name=='Techno Kallos'?<button  onClick={togglePopup} className="relative px-6 py-2.5 font-medium text-white group">
-        <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-violet-200 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-        <span className="absolute inset-0 w-full h-full border-2 bg-violet-800 group-hover:bg-violet-600"></span>
-        <span className="relative text-white group-hover:text-white">View Qualified Team Details</span>
-      </button>:<a href="https://rajalakshmi.org/design-summit-2k24/" target="_self" rel="noopener noreferrer">
-      <button className="relative px-6 py-2.5 font-medium text-white group">
-        <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-violet-200 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-        <span className="absolute inset-0 w-full h-full border-2 bg-violet-800 group-hover:bg-violet-600"></span>
-        <span className="relative text-white group-hover:text-white">Register</span>
-      </button>
-      </a>
-}   */}
-
-{isPopupOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 overflow-scroll">
-         <div className="bg-white w-[95%] p-2 rounded-lg shadow-lg relative">
-  <h2 className="text-xl font-semibold mb-4 ml-4 p-2 mx-auto">Qualified Team Details</h2>
-  <ul className="list-decimal list-inside text-left grid grid-cols-2 ">
-    {[
-      "Tech Titans",
-      "Tech Fury",
-      "HackOverflow",
-      "Binary Brains",
-      "Design Sprinters",
-      "Tech Titans",
-      "DH",
-      "Flash&Code",
-      "Web Spartenz",
-      "Task Master",
-      "Code freaks",
-      "Hell Divers",
-      "Pixel Pirates",
-      "Section 13",
-      "Sparkoders",
-      "Tech Titans",
-      "Team DS",
-      "Trex-Dino",
-      "WEB WIZARDS",
-      "Binary brains",
-      "FrontEndTheriyathu",
-      "Technoglitz",
-      "NPC's",
-      "Area 404",
-      "DEAD&LOGAN",
-      "Summit seeker",
-      "Web Wave",
-      "Data_Dynamos",
-      "Echo",
-      "Vivaia",
-    ].map((team, index) => (
-      <li key={index} className="p-1 text-black">
-        {team}
-      </li>
-    ))}
-  </ul>
-  <button
-    className="z-[1000] absolute cursor-pointer top-3 right-3 text-3xl text-gray-900 hover:text-gray-400"
-    onClick={togglePopup}
-  >
-    &times;
-  </button>
-</div>
-
+      {/* Rules Popup */}
+      {isPopupOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-white w-[90%] md:w-[60%] p-6 rounded-lg shadow-lg relative">
+            <img src={event.rulesImage} alt="Rules" className="w-full h-auto rounded-md" />
+            <button
+              className="z-[1000] absolute top-3 right-3 text-3xl text-gray-900 hover:text-gray-400"
+              onClick={togglePopup}
+            >
+              &times;
+            </button>
+          </div>
+          {/* Background overlay */}
           <div
             className="fixed inset-0 bg-black opacity-20"
             onClick={togglePopup}
@@ -132,6 +71,8 @@ const EventCard = React.memo(({ event, index }) => {
     </div>
   );
 });
+
+
 const Cardlayout = () => {
 
   const eventGuidelines = [
@@ -142,8 +83,9 @@ const Cardlayout = () => {
           name: "Career Craft",
           day: "Day 1",
           time: "10:00 AM - 12:00 PM",
-          about: "Students will be allowed to clarify their doubts on placements and higher studies from the presenters.",
+          // about: "Students will be allowed to clarify their doubts on placements and higher studies from the presenters.",
           image: "/1.png",
+          rulesImage:'/r1.png'
         },
         {
           name: "Techno Kallos",
@@ -157,6 +99,7 @@ const Cardlayout = () => {
           ],
           judgingCriteria: ["Creativity and Innovation", "Visual Appeal", "Functionality and Feasibility"],
           image: "/2.png",
+          rulesImage:'/r2.png'
         },
         {
           name: "Design Arena",
@@ -174,6 +117,7 @@ const Cardlayout = () => {
           judgingCriteria: ["Creativity and Innovation", "User Experience (UX) Design Principles", "Visual Appeal", "Functionality and Feasibility"],
           judges: "A panel of experienced UI/UX designers will evaluate submissions.",
           image: "/7.png",
+          rulesImage:'/r3.png'
         },
         {
           name: "Vision Vault",
@@ -189,6 +133,7 @@ const Cardlayout = () => {
           ],
           judgingCriteria: ["Presentation should be clear, compelling and crafted.", "Feasibility and scalability of the project.", "Presentation and communication skills."],
           image: "/3.png",
+          rulesImage:'/r4.png'
         },
         {
           name: "Discussion Den",
@@ -205,6 +150,7 @@ const Cardlayout = () => {
           ],
           judgingCriteria: ["Clarity of Thought", "Team Collaboration and Dynamics", "Relevance and Depth of Discussion", "Presentation Skills"],
           image: "/4.png",
+          rulesImage:'/r5.png'
         },
         {
           name: "Code Crack",
@@ -218,6 +164,7 @@ const Cardlayout = () => {
             "No plagiarism.",
           ],
           image: "/5.png",
+          rulesImage:'/r6.png'
         },
       ],
     },
@@ -238,10 +185,11 @@ const Cardlayout = () => {
           ],
           materialsProvided: ["Chairs and tables", "A black or whiteboard for brainstorming", "Pens and papers for notes"],
           image: "/6.png",
+          rulesImage:'/r7.png'
         }
       ]
     },
-  ];
+  ]; 
   return (
     <div className='z-10 max-h-fit w-full bg-black flex flex-col items-center'>
       <div className='text-4xl md:text-6xl w-full text-center text-white font-bold py-8'>
@@ -250,7 +198,11 @@ const Cardlayout = () => {
       <div className="w-full max-w-fit mx-auto flex flex-wrap justify-center gap-12 p-10">
         {eventGuidelines.map((category, catIndex) => (
           category.events.map((event, eventIndex) => (
-            <EventCard key={`${catIndex}-${eventIndex}`} event={event} index={catIndex * category.events.length + eventIndex} />
+            <EventCard
+              key={`${catIndex}-${eventIndex}`}
+              event={event}
+              index={catIndex * category.events.length + eventIndex}
+            />
           ))
         ))}
       </div>
